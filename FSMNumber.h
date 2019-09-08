e57ac1a02f0982b1226df9fe435f66ae3f050efd
apoggi-verignon <54695667+apoggi-verignon@users.noreply.github.com>
2019-09-08 15:14:30 -0300

    Add files via upload

---
+++ FSMNumber.h
@@ -0,0 +1,43 @@
+#ifndef _FSMNUMBER_H
+#define _FSMNUMBER_H
+
+#include "genericFSM.h"
+
+enum{state0, state1, state2, state3, state4, state5, state6, state7, state8};
+
+class FSMNumber:public genericFSM{
+
+public:
+    FSMNumber():genericFSM(){}
+
+private:
+#define TX(x) (static_cast<void (genericFSM::* )(eventType*)>(&FSMNumber::x))
+
+    const fsmCell fsmTable[11][8] = {
+
+            // Recibir -            // Recibir 0            // Recibir digit        // Recibir .            // Recibir e            // Recibir E            // Recibir +                //Recibir fin
+            {state1, TX(prueba0)},  {state2, TX(prueba1)},  {state3, TX(prueba1)},  {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {state1, TX(prueba1)},      {ERROR, TX(prueba1)},
+            {ERROR, TX(prueba1)},	{state2, TX(prueba1)},  {state3, TX(prueba1)},  {ERROR, TX(prueba0)},   {ERROR, TX(prueba0)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},       {ERROR, TX(prueba1)},
+            {ERROR, TX(prueba0)},	{ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {state4, TX(prueba1)},  {state6, TX(prueba1)},  {state6, TX(prueba1)},  {ERROR, TX(prueba0)},       {FIN, TX(prueba1)},
+            {ERROR, TX(prueba1)},	{state3, TX(prueba1)}, 	{state3, TX(prueba1)},  {state4, TX(prueba1)},	{ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},       {FIN, TX(prueba1)},
+            {ERROR, TX(prueba1)},   {state5, TX(prueba1)},  {state5, TX(prueba1)},  {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},       {ERROR, TX(prueba1)},
+            {ERROR, TX(prueba1)},   {state5, TX(prueba1)},  {state5, TX(prueba1)},  {ERROR, TX(prueba1)},   {state6, TX(prueba1)},  {state6, TX(prueba1)},  {ERROR, TX(prueba1)},       {FIN, TX(prueba1)},
+            {state7, TX(prueba1)},  {state8, TX(prueba1)},  {state8, TX(prueba1)},  {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {state7, TX(prueba1)},      {ERROR, TX(prueba1)},
+            {ERROR, TX(prueba1)},   {state8, TX(prueba1)},  {state8, TX(prueba1)},  {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},       {ERROR, TX(prueba1)},
+            {ERROR, TX(prueba1)},   {state8, TX(prueba1)},  {state8, TX(prueba1)},  {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},       {FIN, TX(prueba1)},
+            {}, {}, {}, {}, {}, {}, {}, // Error
+            {}, {}, {}, {}, {}, {}, {}, // Fin
+    };
+
+    void prueba0(eventType* ev){
+        int a=0;
+        a++;
+    }
+
+    void prueba1(eventType* ev){
+        int a=0;
+        a++;
+    }
+};
+
+#endif //_FSMNUMBER_H
---
+++ FSMString.h
@@ -0,0 +1,39 @@
+#ifndef _FSMSTRING_H
+#define _FSMSTRING_H
+
+#include "genericFSM.h"
+
+enum{state0, state1, state2, state3, state4};
+
+class FSMString:public genericFSM{
+
+public:
+    FSMString():genericFSM(){}
+
+private:
+#define TX(x) (static_cast<void (genericFSM::* )(eventType*)>(&FSMString::x))
+
+    const fsmCell fsmTable[7][6] = {
+
+            // Recibir "            // Recibir char         // Recibir \            // Recibir u            // Recibir 4hex             // Recibir symb
+            {state1, TX(prueba0)},  {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},      {ERROR, TX(prueba1)},
+            {FIN, TX(prueba1)},	    {state1, TX(prueba1)},  {state2, TX(prueba1)},  {state1, TX(prueba0)},  {state1, TX(prueba0)},     {state1, TX(prueba1)},
+            {state3, TX(prueba0)},	{ERROR, TX(prueba1)},   {state3, TX(prueba1)},   {state4, TX(prueba1)},  {ERROR, TX(prueba1)},     {state3, TX(prueba1)},
+            {FIN, TX(prueba1)},	    {state1, TX(prueba1)}, 	{state2, TX(prueba1)},  {state1, TX(prueba1)},	{state1, TX(prueba1)},     {state1, TX(prueba1)},
+            {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {ERROR, TX(prueba1)},   {state3, TX(prueba1)},     {ERROR, TX(prueba1)},
+            {}, {}, {}, {}, {}, {}, // Error
+            {}, {}, {}, {}, {}, {} // Fin
+    };
+
+    void prueba0(eventType* ev){
+        int a=0;
+        a++;
+    }
+
+    void prueba1(eventType* ev){
+        int a=0;
+        a++;
+    }
+};
+
+#endif //_FSMSTRING_H
